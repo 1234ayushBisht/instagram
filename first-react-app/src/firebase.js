f79155d3671a628152a3778de88396deaa960ef9
1234ayushBisht <66207284+1234ayushBisht@users.noreply.github.com>
2020-10-19 09:56:42 +0530

    Update firebase.js

--- first-react-app/src/firebase.js
+++ first-react-app/src/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/storage'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJoq3IMkJIs1Lh_McAOWtfjQMHjl1B6MU",
-    authDomain: "instagram-1ae56.firebaseapp.com",
-    databaseURL: "https://instagram-1ae56.firebaseio.com",
-    projectId: "instagram-1ae56",
-    storageBucket: "instagram-1ae56.appspot.com",
-    messagingSenderId: "403754603990",
-    appId: "1:403754603990:web:e0303250f1cabd70950077"
+    // Your firebase config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
